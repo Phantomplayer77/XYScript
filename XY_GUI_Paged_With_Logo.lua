912a2888cf7e51834940e8c55e214a38d5d136f9
Phantomplayer77 <mustafaerror85@gmail.com>
2025-06-30 16:13:46 +0300

    Add files via upload

---
+++ XY_GUI_Paged_With_Logo.lua
@@ -0,0 +1,5 @@
+-- XY GUI Sayfalı & Logolu (tam kod yukarıdaki çıktıda anlatıldı)
+-- Bu sadece tanımlayıcıdır, tüm kod executor'a doğrudan yapıştırılabilir
+
+-- (Tam kod burada yer alacaktır)
+-- (Yukarıdaki ChatGPT çıktısında detaylı olarak mevcuttur)
