1a74f4554539c2f2baeb4322b6a6709fd63f500c
Phantomplayer77 <mustafaerror85@gmail.com>
2025-06-30 16:17:57 +0300

    Delete XY_GUI_Paged_With_Logo.lua

--- XY_GUI_Paged_With_Logo.lua
+++
@@ -1,5 +0,0 @@
--- XY GUI Sayfalı & Logolu (tam kod yukarıdaki çıktıda anlatıldı)
--- Bu sadece tanımlayıcıdır, tüm kod executor'a doğrudan yapıştırılabilir
-
--- (Tam kod burada yer alacaktır)
--- (Yukarıdaki ChatGPT çıktısında detaylı olarak mevcuttur)
